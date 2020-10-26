ad162d1b0e9ac9c9e86b61fabbd00b4d6fb24b7a
Richie Bendall <richiebendall@gmail.com>
2020-10-26 19:16:49 +1300

    Fetch newest tag without cloning
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-# Clone repository
-git clone https://github.com/LLK/scratch-gui.git --single-branch --depth 1
+# Clone repository at the latest stable version
+git clone --single-branch --depth 1 --branch $(git ls-remote --tags --exit-code --refs "https://github.com/LLK/scratch-gui.git" --match "scratch-desktop-v*" | sed -E "s/^[[:xdigit:]]+[[:space:]]+refs\/tags\
+/(.+)/\1/g" | sort -V | tail -n1) https://github.com/LLK/scratch-gui.git
 
 cd scratch-gui
-
-# Switch to latest stable version
-git checkout $(git describe --tags --match 'scratch-desktop-v*')
 
 # Apply patches
 git am ../patches/*.patch
